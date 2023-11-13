7ad3b43f213899435148e5dc52410114c8e56c34
asimsedhain <asimsedhain33@gmail.com>
2023-11-12 23:57:32 -0600

    chore: switch to make file

--- scripts/bench.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cargo build --release
-
-RUN_SIZE=10000000
-
-hyperfine --warmup 3 "./target/release/naive-threads-rtrb $RUN_SIZE" "./target/release/naive-threads-std $RUN_SIZE" "./target/release/naive $RUN_SIZE" "./target/release/thread-multi-join $RUN_SIZE"
